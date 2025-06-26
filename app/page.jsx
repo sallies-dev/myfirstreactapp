df17fc379c3e4a9ec494f59fdd81844fadcf1470
Paul salifu <salifupaul48@gmail.com>
2025-06-26 13:44:10 +0100

    added a few variables

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ const page = () => {
   return (
     <div className='flex items-center justify-center h-dvh'>
        <h1 className='text-3xl font-bold text-blue-600'>This Is My Main Branch</h1>
+       <h2>Anything of ur choice</h2>
     </div>
   )
 }
