360041b94fff0604bfe4f933ec8b954cbd1f7058
Paul salifu <salifupaul48@gmail.com>
2025-06-26 12:47:17 +0100

    bg color

---
+++ app/courses/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      courses
+      <h1 className='bg-red-600 text-white'>Hello courses</h1>
+    </>
+  )
+}
+
+export default page
