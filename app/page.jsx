1c90f0063e08f3c997c90f82529ee08fb0c4da33
Paul salifu <salifupaul48@gmail.com>
2025-06-26 13:32:05 +0100

    changed my div again

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const page = () => {
   return (
     <div className='flex items-center justify-center h-dvh'>
-       <h1 className='text-3xl font-bold text-blue-600'>This Is My Main Branch</h1>
+       <h1 className='text-3xl font-bold text-red-600'>
+        This Is My Test Branch
+        </h1>
     </div>
   )
 }
