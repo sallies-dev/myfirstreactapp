d46aac183a322324f5cd06fdccf28d530fea3b64
Paul salifu <salifupaul48@gmail.com>
2025-06-26 13:21:00 +0100

    styled div&h1

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-       hello
+    <div className='flex items-center justify-center h-dvh'>
+       <h1 className='text-3xl font-bold text-blue-600'>This Is My Main Branch</h1>
     </div>
   )
 }
